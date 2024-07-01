8b3bea875662d50fb71a2144316edf3c80268551
gbrolly2 <136117583+gbrolly2@users.noreply.github.com>
2024-07-01 09:00:45 -0500

    Update forms.py

--- contraception_app/forms.py
+++ contraception_app/forms.py
@@ -6,6 +6,12 @@ class QuestionnaireForm(forms.Form):
         ('yes', 'Yes'),
         ('no', 'No'),
     ]
+    
+    YES_NO_PREFER_NOT_TO_ANSWER_CHOICES = [
+        ('yes', 'Yes'),
+        ('no', 'No'),
+        ('prefer_not_to_answer', 'Prefer not to answer')
+    ]
 
     QUESTION4_CHOICES = [
         ('a', '35 years or older and smoke'),
@@ -42,12 +48,12 @@ class QuestionnaireForm(forms.Form):
     question9 = forms.ChoiceField(label='9. Do you have a copper allergy?', choices=YES_NO_CHOICES, widget=forms.RadioSelect)
     question10 = forms.ChoiceField(label='10. Do you have an allergy to silicone?', choices=YES_NO_CHOICES, widget=forms.RadioSelect)
     question11 = forms.ChoiceField(label='11. Have you ever been diagnosed with an anatomical abnormality of the uterus?', choices=YES_NO_CHOICES, widget=forms.RadioSelect)
-    question12 = forms.ChoiceField(label='12. Have you ever been diagnosed with a pelvic infection (PID)?', choices=YES_NO_CHOICES, widget=forms.RadioSelect)
-    question13 = forms.ChoiceField(label='13. Do you have a current sexually transmitted infection (STI)?', choices=YES_NO_CHOICES, widget=forms.RadioSelect)
-    question14 = forms.ChoiceField(label='14. Have you ever been diagnosed with cervical cancer?', choices=YES_NO_CHOICES, widget=forms.RadioSelect) 
-    question15 = forms.ChoiceField(label='15. Do you have a history of HIV/AIDs?', choices=YES_NO_CHOICES, widget=forms.RadioSelect)
-    question16 = forms.ChoiceField(label='16. Are you on hormone replacement therapy for gender-affirming care?', choices=YES_NO_CHOICES, widget=forms.RadioSelect)
+    question12 = forms.ChoiceField(label='12. Have you ever been diagnosed with a pelvic infection (PID)?', choices=YES_NO_PREFER_NOT_TO_ANSWER_CHOICES, widget=forms.RadioSelect)
+    question13 = forms.ChoiceField(label='13. Do you have a current sexually transmitted infection (STI)?', choices=YES_NO_PREFER_NOT_TO_ANSWER_CHOICES, widget=forms.RadioSelect)
+    question14 = forms.ChoiceField(label='14. Have you ever been diagnosed with cervical cancer?', choices=YES_NO_PREFER_NOT_TO_ANSWER_CHOICES, widget=forms.RadioSelect)
+    question15 = forms.ChoiceField(label='15. Do you have a history of HIV/AIDs?', choices=YES_NO_PREFER_NOT_TO_ANSWER_CHOICES, widget=forms.RadioSelect)
+    question16 = forms.ChoiceField(label='16. Are you on hormone replacement therapy for gender-affirming care?', choices=YES_NO_PREFER_NOT_TO_ANSWER_CHOICES, widget=forms.RadioSelect)
     question17 = forms.ChoiceField(label='17. Do you prefer hormonal birth control methods?', choices=YES_NO_CHOICES, widget=forms.RadioSelect)
     question18 = forms.ChoiceField(label='18. Do you have any interest in getting your tubes tied?', choices=YES_NO_CHOICES, widget=forms.RadioSelect)
-    question19 = forms.ChoiceField(label='19. Are you consistent with taking daily pills? ', choices=YES_NO_CHOICES, widget=forms.RadioSelect)
+    question19 = forms.ChoiceField(label='19. Are you consistent with taking daily pills?', choices=YES_NO_CHOICES, widget=forms.RadioSelect)
     question20 = forms.MultipleChoiceField(label='20. Check off what is most important to you (choose as many as you want)', choices=QUESTION20_CHOICES, widget=forms.CheckboxSelectMultiple)
